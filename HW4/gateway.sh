3b3fc55b8b74b0e37fa3add1359ab286c3d504e9
jd <jdica001@ucr.edu>
2019-05-25 15:23:36 -0700

    networking update

--- HW4/gateway.sh
+++ HW4/gateway.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
 
 ip route | grep 'default' | awk 'BEGIN {ORS=" "} {print "gateway: "$3}' 
-ifconfig | grep 'Bcast' | cut -d: -f4 | awk 'BEGIN {ORS=" "} {print "netmask: " $1}' 
+ifconfig | grep 'broadcast' | cut -d: -f4 | awk 'BEGIN {ORS=" "} {print "netmask: " $4}' 
 ip route | grep 'default' | awk '{print "device: " $5}'
